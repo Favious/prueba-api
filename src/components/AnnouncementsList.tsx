48861c10621e5633a5e0f9d12edfaa26c1144e1a
Favious <faviopaco@gmail.com>
2021-10-13 19:26:48 -0400

    add some styles

--- src/components/AnnouncementsList.tsx
+++ src/components/AnnouncementsList.tsx
@@ -6,181 +6,124 @@ import AnnouncementData from "../types/announcement.type";
 type Props = {};
 
 type State = {
-    announcements: Array<AnnouncementData>,
-    currentAnnouncement: AnnouncementData | null,
-    currentIndex: number,
-    searchTitle: string
+  announcements: Array<AnnouncementData>;
+  currentAnnouncement: AnnouncementData | null;
+  currentIndex: number;
+  searchTitle: string;
 };
 
-export default class AnnouncementsList extends Component<Props, State>{
-    constructor(props: Props) {
-        super(props);
-        this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
-        this.retrieveAnnouncements = this.retrieveAnnouncements.bind(this);
-        this.refreshList = this.refreshList.bind(this);
-        this.setActiveAnnouncement = this.setActiveAnnouncement.bind(this);
-        this.removeAllAnnouncements = this.removeAllAnnouncements.bind(this);
-        this.searchTitle = this.searchTitle.bind(this);
+export default class AnnouncementsList extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+    this.retrieveAnnouncements = this.retrieveAnnouncements.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveAnnouncement = this.setActiveAnnouncement.bind(this);
+    this.removeAllAnnouncements = this.removeAllAnnouncements.bind(this);
+    this.searchTitle = this.searchTitle.bind(this);
 
-        this.state = {
-            announcements: [],
-            currentAnnouncement: null,
-            currentIndex: -1,
-            searchTitle: ""
-        };
-    }
+    this.state = {
+      announcements: [],
+      currentAnnouncement: null,
+      currentIndex: -1,
+      searchTitle: "",
+    };
+  }
 
-    componentDidMount() {
-        this.retrieveAnnouncements();
-    }
+  componentDidMount() {
+    this.retrieveAnnouncements();
+  }
 
-    onChangeSearchTitle(e: ChangeEvent<HTMLInputElement>) {
-        const searchTitle = e.target.value;
+  onChangeSearchTitle(e: ChangeEvent<HTMLInputElement>) {
+    const searchTitle = e.target.value;
 
+    this.setState({
+      searchTitle: searchTitle,
+    });
+  }
+
+  retrieveAnnouncements() {
+    AnnouncementDataService.getAll()
+      .then((response) => {
         this.setState({
-            searchTitle: searchTitle
+          announcements: response.data,
         });
-    }
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
-    retrieveAnnouncements() {
-        AnnouncementDataService.getAll()
-            .then(response => {
-                this.setState({
-                    announcements: response.data
-                });
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
+  refreshList() {
+    this.retrieveAnnouncements();
+    this.setState({
+      currentAnnouncement: null,
+      currentIndex: -1,
+    });
+  }
 
-    refreshList() {
-        this.retrieveAnnouncements();
+  setActiveAnnouncement(announcement: AnnouncementData, index: number) {
+    this.setState({
+      currentAnnouncement: announcement,
+      currentIndex: index,
+    });
+  }
+
+  removeAllAnnouncements() {
+    AnnouncementDataService.deleteAll()
+      .then((response) => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  searchTitle() {
+    this.setState({
+      currentAnnouncement: null,
+      currentIndex: -1,
+    });
+
+    AnnouncementDataService.findByTitle(this.state.searchTitle)
+      .then((response) => {
         this.setState({
-            currentAnnouncement: null,
-            currentIndex: -1
+          announcements: response.data,
         });
-    }
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
-    setActiveAnnouncement(announcement: AnnouncementData, index: number) {
-        this.setState({
-            currentAnnouncement: announcement,
-            currentIndex: index
-        });
-    }
+  render() {
+    const { searchTitle, announcements, currentAnnouncement, currentIndex } =
+      this.state;
 
-    removeAllAnnouncements() {
-        AnnouncementDataService.deleteAll()
-            .then(response => {
-                console.log(response.data);
-                this.refreshList();
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
-
-    searchTitle() {
-        this.setState({
-            currentAnnouncement: null,
-            currentIndex: -1
-        });
-
-        AnnouncementDataService.findByTitle(this.state.searchTitle)
-            .then(response => {
-                this.setState({
-                    announcements: response.data
-                });
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
-
-    render() {
-        const { searchTitle, announcements, currentAnnouncement, currentIndex } = this.state;
-
-        return (
-            <div className="list row">
-                <div className="col-md-8">
-                    <div className="input-group mb-3">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Search by title"
-                            value={searchTitle}
-                            onChange={this.onChangeSearchTitle}
-                        />
-                        <div className="input-group-append">
-                            <button
-                                className="btn btn-outline-secondary"
-                                type="button"
-                                onClick={this.searchTitle}
-                            >
-                                Search
-                            </button>
-                        </div>
-                    </div>
+    return (
+      <div className="container p-3">
+        {announcements &&
+          announcements.map((announcement: AnnouncementData, index: number) => (
+            <div className="row mx-0 mb-2">
+              <div className="col-8 offset-2 btn-info btn-md announcement">
+                <div className="row">
+                  <div className="col-xs-12 col-md-6 col-lg-4">
+                    {announcement.titulo}
+                  </div>
+                  <div className="col-md-2 col-lg-4 d-none d-lg-block text-end">
+                    {announcement.codigo}
+                  </div>
+                  <div className="col-md-4 col-lg-4 d-none d-md-block text-end">
+                    {announcement.consultor}
+                  </div>
                 </div>
-                <div className="col-md-6">
-                    <h4>Tutorials List</h4>
-
-                    <ul className="list-group">
-                        {announcements &&
-                        announcements.map((announcement: AnnouncementData, index: number) => (
-                            <li
-                                className={
-                                    "list-group-item " +
-                                    (index === currentIndex ? "active" : "")
-                                }
-                                onClick={() => this.setActiveAnnouncement(announcement, index)}
-                                key={index}
-                            >
-                                {announcement.title}
-                            </li>
-                        ))}
-                    </ul>
-
-                    <button
-                        className="m-3 btn btn-sm btn-danger"
-                        onClick={this.removeAllAnnouncements}
-                    >
-                        Remove All
-                    </button>
-                </div>
-                <div className="col-md-6">
-                    {currentAnnouncement? (
-                        <div>
-                            <h4>Tutorial</h4>
-                            <div>
-                                <label>
-                                    <strong>Title:</strong>
-                                </label>{" "}
-                                {currentAnnouncement.title}
-                            </div>
-                            <div>
-                                <label>
-                                    <strong>Code:</strong>
-                                </label>{" "}
-                                {currentAnnouncement.code}
-                            </div>
-                            <div>
-                                <label>
-                                    <strong>Consultant:</strong>
-                                </label>{" "}
-                                {currentAnnouncement.consultant}
-                            </div>
-                        </div>
-                    ) : (
-                        <div>
-                            <br />
-                            <p>Please click on a Tutorial...</p>
-                        </div>
-                    )}
-                </div>
+              </div>
             </div>
-        );
-    }
+          ))}
+      </div>
+    );
+  }
 }
--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/convocatorias",
+  baseURL: "http://localhost:8000/api/",
   headers: {
     "Content-type": "application/json",
   },
